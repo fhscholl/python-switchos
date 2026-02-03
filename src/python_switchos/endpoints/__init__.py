02ee751fb1246a5e03ee62743686b9e5eac2e890
Frank Scholl <fhscholl@gmail.com>
2026-02-03 13:28:27 -0500

    feat(03-01): create SfpEndpoint dataclass with 11 fields
    - Add SfpEndpoint with vendor, part_number, revision, serial, date, type info fields
    - Add temperature, voltage, tx_bias, tx_power, rx_power diagnostic fields
    - Export SfpEndpoint from endpoints package

---
+++ src/python_switchos/endpoints/__init__.py
@@ -0,0 +1,5 @@
+from python_switchos.endpoints.link import LinkEndpoint
+from python_switchos.endpoints.sfp import SfpEndpoint
+from python_switchos.endpoints.sys import SystemEndpoint
+
+__all__ = ["LinkEndpoint", "SfpEndpoint", "SystemEndpoint"]
